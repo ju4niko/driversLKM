3c9d6b5c4c0c6fe591cbb3140362a3b40b578dd1
jmontenegro <ju4niko@gmail.com>
2023-10-06 14:45:44 -0300

    agregado peek de task_struct

--- LKM-hello.c
+++ LKM-hello.c
@@ -10,12 +10,16 @@ MODULE_DESCRIPTION("HolaMundo LKM");
 static int __init hello_init(void) {
 
   printk(KERN_ALERT "Hola mundo Kernel!. Soy un driver!\n");
+  printk(KERN_ALERT "desde el PID:%d...\n",current->pid);
+  printk(KERN_ALERT "Nombre del programa: %s\n", current->mm->exe_file->f_path.dentry->d_name.name);
   return 0; 
 }
 
 static void __exit hello_exit(void) {
 
   printk(KERN_ALERT "Adios mundo Kernel... \n"); 
+  printk(KERN_ALERT "Nombre del programa: %s\n", current->mm->exe_file->f_path.dentry->d_name.name);
+  printk(KERN_ALERT "El proceso actual no tiene un programa asociado.\n");
 }
 
 module_init(hello_init); 
